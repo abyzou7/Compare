634a89731ba28ee2a01fe58abc707155be2363f0
Kassim Mohamed <55437992+abyzou7@users.noreply.github.com>
2022-04-27 15:21:28 +0200

    Update CompareScripts.js

--- CompareScripts.js
+++ CompareScripts.js
@@ -1,35 +1,3 @@
-var Webflow = Webflow || [];
-Webflow.push(function () {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.defer = 'defer';
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.1/jspdf.min.js';
-    var script2 = document.createElement('script');
-    script2.type = 'text/javascript';
-    script2.defer = 'defer';
-    script2.src = 'https://html2canvas.hertzen.com/dist/html2canvas.min.js';
-    document.head.appendChild(script);
-    document.head.appendChild(script2);
-    function demoFromHTML() {
-        var doc = new jsPDF({
-            format: "a4"
-        });
-        var width = doc.internal.pageSize.width;
-        var height = doc.internal.pageSize.height;
-        html2canvas(document.getElementById("content"),{scrollY: -window.scrollY,scrollX: -window.scrollX}, {
-            scale: "3",
-            quality: "3"
-            }).then(canvas => {
-                this.imgFile = canvas;
-                doc.addImage(this.imgFile, "PNG", 0, 0,width);
-                doc.save("Comparatif" + ".pdf");		
-        			});        
-    }
-    //call the function
-    $('#generate').click(function () {
-        demoFromHTML();
-    });
-});
 
   var url_string = window.location.href;
   var url = new URL(url_string);
